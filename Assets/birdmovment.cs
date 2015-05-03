74f43be0518bee678a7b460684c842045bb6e446
umairsikander <alltechsource1234@gmail.com>
2015-05-03 13:29:25 +0500

    bird movement flappy bird

---
+++ Assets/birdmovment.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class birdmovment : MonoBehaviour {
+	Vector3 velocity= Vector3.zero;
+	public Vector3 gravity;
+	public Vector3 flapVelocity;
+
+
+	bool didFlap= false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	//Do graphic and input updates
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.S)|| Input.GetMouseButtonDown(0))
+		{
+			didFlap=true;
+		}
+	}
+	
+	// Do physics engine updates here
+	void FixedUpdate () {
+		velocity+=gravity * Time.deltaTime;
+
+		if(didFlap== true)
+		{
+			didFlap = false;
+			velocity+=flapVelocity;
+		}
+		transform.position+= velocity * Time.deltaTime;
+
+	}
+}
