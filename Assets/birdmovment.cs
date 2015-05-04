2ffe2802ebd82d3b6241934c689d02d737ffb89e
umairsikander <alltechsource1234@gmail.com>
2015-05-04 23:27:27 +0500

    Bird rotation in progress

--- Assets/birdmovment.cs
+++ Assets/birdmovment.cs
@@ -5,7 +5,7 @@ public class birdmovment : MonoBehaviour {
 	Vector3 velocity= Vector3.zero;
 	public Vector3 gravity;
 	public Vector3 flapVelocity;
-
+	public float maxSpeed= 5f;
 
 	bool didFlap= false;
 	// Use this for initialization
@@ -29,7 +29,14 @@ public class birdmovment : MonoBehaviour {
 			didFlap = false;
 			velocity+=flapVelocity;
 		}
+		velocity = Vector3.ClampMagnitude(velocity,maxSpeed);
 		transform.position+= velocity * Time.deltaTime;
+		float angle = 0;
+		if(velocity.y<0)
+		{
+		 angle = Mathf.Lerp(0,-90,velocity.y/maxSpeed);
+		}
+		transform.rotation = Quaternion.Euler(0,0,angle);
 
 	}
 }
