12296118772baad8dd1e483dfe5b6bec9d6ab2ca
umairsikander <alltechsource1234@gmail.com>
2015-05-05 19:59:37 +0500

    background camera moving along with bird.

---
+++ Assets/Cameratrackplayer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cameratrackplayer : MonoBehaviour {
+
+	Transform player;
+	float offsetX=0;
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject player_go = GameObject.FindGameObjectWithTag("Player");
+
+		if(player_go == null)
+		{
+			Debug.LogError("Could'nt find the object with tag 'Player'!");
+			return;
+		}
+		player = player_go.transform;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(player != null)
+		{
+			Vector3 pos = transform.position;
+			pos.x = player.position.x + offsetX;
+			transform.position= pos;
+
+		}
+
+
+
+
+	
+	}
+}
--- Assets/birdmovment.cs
+++ Assets/birdmovment.cs
@@ -6,7 +6,8 @@ public class birdmovment : MonoBehaviour {
 	public Vector3 gravity;
 	public Vector3 flapVelocity;
 	public float maxSpeed= 5f;
-
+	public float forwardSpeed = 1f;
+	
 	bool didFlap= false;
 	// Use this for initialization
 	void Start () {
@@ -22,6 +23,7 @@ public class birdmovment : MonoBehaviour {
 	
 	// Do physics engine updates here
 	void FixedUpdate () {
+		velocity.x = forwardSpeed;
 		velocity+=gravity * Time.deltaTime;
 
 		if(didFlap== true)
@@ -29,14 +31,16 @@ public class birdmovment : MonoBehaviour {
 			didFlap = false;
 			velocity+=flapVelocity;
 		}
-		velocity = Vector3.ClampMagnitude(velocity,maxSpeed);
-		transform.position+= velocity * Time.deltaTime;
-		float angle = 0;
-		if(velocity.y<0)
-		{
-		 angle = Mathf.Lerp(0,-90,velocity.y/maxSpeed);
-		}
-		transform.rotation = Quaternion.Euler(0,0,angle);
+//		velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
 
+		transform.position+= velocity * Time.deltaTime;
+
+//		float angle = 0;
+//		if(velocity.y < 0)
+//		{
+//			angle = Mathf.Lerp(0, -90, -velocity.y / maxSpeed);
+//		}
+//	
+//		transform.rotation = Quaternion.Euler(0, 0, angle);
 	}
 }
